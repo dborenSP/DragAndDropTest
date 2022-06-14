391a31640be1bdef86d279d319b8a7bba087ceeb
Daniel Boren <dboren@sunpowercorp.com>
2022-06-14 11:32:05 -0500

    Added dragover, dragenter, dragleave, and drop events to script. Panel element is now movable but I think I'm missing some styling that would make it more intuitive for users

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ document.addEventListener("dragstart", event => {
 document.addEventListener("dragend", event => {
   event.target.classList.remove("dragging");
 });
+
+document.addEventListener("dragover", event => {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", event => {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.classList.contains("dropzone")) {
+    event.target.classList.add("dragover");
+  }
+});
+
+document.addEventListener("dragleave", event => {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.classList.contains("dropzone")) {
+    event.target.classList.remove("dragover");
+  }
+});
+
+document.addEventListener("drop", event => {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged element to the selected drop target
+  if (event.target.classList.contains("dropzone")) {
+    event.target.classList.remove("dragover");
+    dragged.parentNode.removeChild(dragged);
+    event.target.appendChild(dragged);
+  }
+});
